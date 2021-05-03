237faeee36e4f8ea5db07ece1a7d5e79945febf1
AlexSys65 <alexsystems@mail.ru>
2021-05-03 18:30:36 +1100

    Fixed bug with date formatting

--- app/src/main/java/ru/razuvaev/android_one/repository/NoteRepository.java
+++ app/src/main/java/ru/razuvaev/android_one/repository/NoteRepository.java
@@ -1,22 +1,25 @@
 package ru.razuvaev.android_one.repository;
 
-import android.os.Build;
+import android.annotation.SuppressLint;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
-import androidx.annotation.RequiresApi;
+
 
 public class NoteRepository {
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
+    //@RequiresApi(api = Build.VERSION_CODES.O)
     public static ArrayList<Note> getNotes() {
         ArrayList<Note> notes = new ArrayList<>();
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy HH:mm:ss");
+        // DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy HH:mm:ss");
+        @SuppressLint("SimpleDateFormat")
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy HH:mm:ss");
+
         for (int i = 0; i < 20; i++) {
-            notes.add(new Note(LocalDateTime.now().format(formatter), i + "-я ЗАМЕТКА. C помощью метода finish() можно завершить работу активности. " +
+            notes.add(new Note(dateFormat.format(new Date()), i + "-я ЗАМЕТКА. C помощью метода finish() можно завершить работу активности. " +
                     "Если приложение состоит из одной активности, то этого делать не следует, так как система сама завершит работу приложения. " +
                     "Если же приложение содержит несколько активностей, между которыми нужно переключаться, то данный метод позволяет экономить ресурсы. "));
         }
